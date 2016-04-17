0d126d1c64b6b038bf9b2431eb7262257266e5b6
João Marcos Ribeiro Araujo <31223346@mackenzista.com.br>
2016-04-17 05:03:22 -0300

    tela de promocao

--- AngelHack/HomeViewController.swift
+++ AngelHack/HomeViewController.swift
@@ -19,6 +19,7 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         super.viewDidLoad()
         AppLocation.sharedInstance.start()
         tableView.delegate = self
+        tableView.dataSource = self
         barcodeButton.alpha = 0
         tableView.alpha = 0
         msgLbl.alpha = 0
@@ -39,6 +40,7 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         AppData.sharedInstance.delegate = self
         AppNotifications.showLoadingIndicator("Obtendo dados do mercado atual...")
         AppData.getMarkets()
+        AppData.getPromotions()
     }
     
     @IBAction func barcodeButtonPressed(sender: UIButton) {
@@ -50,7 +52,22 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("produto", forIndexPath: indexPath) 
+        let cell = tableView.dequeueReusableCellWithIdentifier("PromotionsTableViewCell", forIndexPath: indexPath) as! PromotionsTableViewCell
+        
+        let promotion = AppData.sharedInstance.promotionsArray![indexPath.row]
+        let product = promotion.product
+        
+        cell.lblPromotion.text = promotion.campaignName
+        
+        let img = product!.image
+        
+        let imgURL = NSURL(string: img!)
+        
+        cell.imgPromotion.af_setImageWithURL(imgURL!, placeholderImage: UIImage(named: "placeholder"))
+        
+        self.view.setNeedsDisplay()
+
+        
         
         return cell
     }
@@ -59,7 +76,6 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return 0.1
     }
 }
-
 
 
 extension HomeViewController: AppDataDelegate {
